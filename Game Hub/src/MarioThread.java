91b62aac883544b82199069a6287d32f35fd0565
nobradley <nobradley@crimson.ua.edu>
2023-06-17 14:11:28 -0400

    Doc for MarioThread

--- Game Hub/src/MarioThread.java
+++ Game Hub/src/MarioThread.java
@@ -1,4 +1,8 @@
-
+/*
+ * The purpose of this class is to have the GamePanel run the MarioGame
+ * on a separate thread to eliminate issues with concurrent running of
+ * the ChessPanel
+ */
 public class MarioThread extends Thread{
 
 	public MarioThread() {
