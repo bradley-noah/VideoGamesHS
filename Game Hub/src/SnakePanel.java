9d21ee9b0bb421befea258753e7c40633f7da4e5
Noah Bradley <noah.oli.bradley@gmail.com>
2023-07-21 00:43:39 -0400

    doc

--- Game Hub/src/SnakePanel.java
+++ Game Hub/src/SnakePanel.java
@@ -1,7 +1,7 @@
 /*
  * The purpose of this class is to create a JPanel, to add to a JFrame
  * It contains the underlying logic behind the entire game
- * the snake, the apples, the game rules, and ending cases
+ * the snake, the apples, the game rules, and ending cases 
  */
 import java.awt.*;
 import java.awt.event.*;
